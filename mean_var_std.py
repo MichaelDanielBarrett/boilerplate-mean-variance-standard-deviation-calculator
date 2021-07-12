cace7f267b25daace9bde8e623178263d136dddd
MichaelDanielBarrett <25422951+MichaelDanielBarrett@users.noreply.github.com>
2021-07-12 17:26:52 -0400

    finished project

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,48 @@ import numpy as np
 
 def calculate(list):
 
+    if len(list) < 9:
+        raise ValueError('List must contain nine numbers.')
 
+    else:
+        
 
+        mtrx = np.array([
+            list[:3],
+            list[3:6],
+            list[6:]
+        ])
 
-    return calculations
+        calculations = dict()
+        calculations['mean'] = [
+            mtrx.mean(axis=0).tolist(),
+            mtrx.mean(axis=1).tolist(),
+            mtrx.mean()
+        ]
+        calculations['variance'] = [
+            mtrx.var(axis=0).tolist(),
+            mtrx.var(axis=1).tolist(),
+            mtrx.var()
+        ]
+        calculations['standard deviation'] = [
+            mtrx.std(axis=0).tolist(),
+            mtrx.std(axis=1).tolist(),
+            mtrx.std()
+        ]
+        calculations['max'] = [
+            mtrx.max(axis=0).tolist(),
+            mtrx.max(axis=1).tolist(),
+            mtrx.max()
+        ]
+        calculations['min'] = [
+            mtrx.min(axis=0).tolist(),
+            mtrx.min(axis=1).tolist(),
+            mtrx.min()
+        ]
+        calculations['sum'] = [
+            mtrx.sum(axis=0).tolist(),
+            mtrx.sum(axis=1).tolist(),
+            mtrx.sum()
+        ]
+
+        return calculations
